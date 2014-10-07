247b1ba68df058a622a7583f2a223f0daef564f1
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-10-07 14:10:06 -0700

    Use :require instead of :use in rules.clj

--- src/datalog/rules.clj
+++ src/datalog/rules.clj
@@ -16,10 +16,10 @@
 ;; Converted to Clojure1.4 by Martin Trojer 2012.
 
 (ns datalog.rules
-  (:use [datalog.util]
-        [datalog.literals]
-        [datalog.database])
-  (:use [clojure.set :only (union intersection difference subset?)]))
+  (:require
+   [clojure.set :as set]
+   [datalog.util :as util]
+   [datalog.literals :as literals]))
 
 (defrecord DatalogRule [head body])
 
@@ -27,13 +27,13 @@
   "Return the rule in a readable format."
   [rule]
   (list* '<-
-         (-> rule :head display-literal)
-         (map display-literal (:body rule))))
+         (-> rule :head literals/display-literal)
+         (map literals/display-literal (:body rule))))
 
 (defn display-query
   "Return a query in a readable format."
   [query]
-  (list* '?- (display-literal query)))
+  (list* '?- (literals/display-literal query)))
 
 ;; =============================
 ;; Check rule safety
@@ -41,11 +41,11 @@
 (defn is-safe?
   "Is the rule safe according to the datalog protocol?"
   [rule]
-  (let [hv (literal-vars (:head rule))
-        bpv (apply union (map positive-vars (:body rule)))
-        bnv (apply union (map negative-vars (:body rule)))
-        ehv (difference hv bpv)
-        env (difference bnv bpv)]
+  (let [hv (literals/literal-vars (:head rule))
+        bpv (apply set/union (map literals/positive-vars (:body rule)))
+        bnv (apply set/union (map literals/negative-vars (:body rule)))
+        ehv (set/difference hv bpv)
+        env (set/difference bnv bpv)]
     (when-not (empty? ehv)
       (throw (Exception. (str "Head vars" ehv "not bound in body of rule" rule))))
     (when-not (empty? env)
@@ -62,10 +62,10 @@
 (defmacro <-
   "Build a datalog rule.  Like this:
 
-   (<- (:head :x ?x :y ?y) (:body-1 :x ?x :y ?y) (:body-2 :z ?z) (not! :body-3 :x ?x) (if > ?y ?z))"
+  (<- (:head :x ?x :y ?y) (:body-1 :x ?x :y ?y) (:body-2 :z ?z) (not! :body-3 :x ?x) (if > ?y ?z))"
   [hd & body]
-  (let [head (build-atom hd :datalog.literals/literal)
-        body (map build-literal body)]
+  (let [head (literals/build-atom hd :datalog.literals/literal)
+        body (map literals/build-literal body)]
     `(is-safe? (build-rule ~head [~@body]))))
 
 (defmethod print-method ::datalog-rule
@@ -80,7 +80,7 @@
 (defmacro ?-
   "Define a datalog query"
   [& q]
-  (let [qq (build-atom q :datalog.literals/literal)]
+  (let [qq (literals/build-atom q :datalog.literals/literal)]
     `(with-meta ~qq {:type ::datalog-query})))
 
 (defmethod print-method ::datalog-query
@@ -92,26 +92,26 @@
 
 (defn compute-sip
   "Given a set of bound column names, return an adorned sip for this
-   rule.  A set of intensional predicates should be provided to
-   determine what should be adorned."
+  rule.  A set of intensional predicates should be provided to
+  determine what should be adorned."
   [bindings i-preds rule]
   (let [next-lit (fn [bv body]
                    (or (first (drop-while
-                               #(not (literal-appropriate? bv %))
+                               #(not (literals/literal-appropriate? bv %))
                                body))
-                       (first (drop-while (complement positive?) body))))
+                       (first (drop-while (complement literals/positive?) body))))
         adorn (fn [lit bvs]
-                (if (i-preds (literal-predicate lit))
-                  (let [bnds (union (get-cs-from-vs lit bvs)
-                                    (get-self-bound-cs lit))]
-                    (adorned-literal lit bnds))
+                (if (i-preds (literals/literal-predicate lit))
+                  (let [bnds (set/union (literals/get-cs-from-vs lit bvs)
+                                    (literals/get-self-bound-cs lit))]
+                    (literals/adorned-literal lit bnds))
                   lit))
-        new-h (adorned-literal (:head rule) bindings)]
-    (loop [bound-vars (get-vs-from-cs (:head rule) bindings)
+        new-h (literals/adorned-literal (:head rule) bindings)]
+    (loop [bound-vars (literals/get-vs-from-cs (:head rule) bindings)
            body (:body rule)
            sip []]
       (if-let [next (next-lit bound-vars body)]
-        (recur (union bound-vars (literal-vars next))
+        (recur (set/union bound-vars (literals/literal-vars next))
                (remove #(= % next) body)
                (conj sip (adorn next bound-vars)))
         (build-rule new-h (concat sip body))))))
@@ -121,7 +121,7 @@
 
 (defn make-rules-set
   "Given an existing set of rules, make it a 'rules-set' for
-   printing."
+  printing."
   [rs]
   (with-meta rs {:type ::datalog-rules-set}))
 
@@ -145,10 +145,10 @@
 
 (defn predicate-map
   "Given a rules-set, return a map of rules keyed by their predicates.
-   Each value will be a set of rules."
+  Each value will be a set of rules."
   [rs]
   (let [add-rule (fn [m r]
-                   (let [pred (-> r :head literal-predicate)
+                   (let [pred (-> r :head literals/literal-predicate)
                          os (get m pred #{})]
                      (assoc m pred (conj os r))))]
     (reduce add-rule {} rs)))
@@ -156,7 +156,7 @@
 (defn all-predicates
   "Given a rules-set, return all defined predicates"
   [rs]
-  (set (map literal-predicate (map :head rs))))
+  (set (map literals/literal-predicate (map :head rs))))
 
 (defn non-base-rules
   "Return a collection of rules that depend, somehow, on other rules"
@@ -164,7 +164,7 @@
   (let [pred (all-predicates rs)
         non-base (fn [r]
                    (if (some #(pred %)
-                             (map literal-predicate (:body r)))
+                             (map literals/literal-predicate (:body r)))
                      r
                      nil))]
     (remove nil? (map non-base rs))))
@@ -176,22 +176,22 @@
 
 (defn apply-rule
   "Apply the rule against db-1, adding the results to the appropriate
-   relation in db-2.  The relation will be created if needed."
+  relation in db-2.  The relation will be created if needed."
   ([db rule] (apply-rule db db rule))
   ([db-1 db-2 rule]
-     (trace-datalog (println)
-                    (println)
-                    (println "--------------- Begin Rule ---------------")
-                    (println rule))
+     (util/trace-datalog (println)
+                         (println)
+                         (println "--------------- Begin Rule ---------------")
+                         (println rule))
      (let [head (:head rule)
            body (:body rule)
            step (fn [bs lit]
-                  (trace-datalog (println bs)
-                                 (println lit))
-                  (join-literal db-1 lit bs))
+                  (util/trace-datalog (println bs)
+                                      (println lit))
+                  (literals/join-literal db-1 lit bs))
            bs (reduce step empty-bindings body)]
-       (do (trace-datalog (println bs))
-           (project-literal db-2 head bs)))))
+       (do (util/trace-datalog (println bs))
+           (literals/project-literal db-2 head bs)))))
 
 (defn apply-rules-set
   [db rs]
